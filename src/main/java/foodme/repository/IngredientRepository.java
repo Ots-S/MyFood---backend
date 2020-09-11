bc5c412beb58c93a64577b09032e488dbc8032dd
Seb Ots <ots.sebastien@gmail.com>
2020-09-11 12:33:17 +0200

    feat check existing ingredient before save

--- src/main/java/foodme/repository/IngredientRepository.java
+++ src/main/java/foodme/repository/IngredientRepository.java
@@ -1,9 +1,18 @@
 package foodme.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import foodme.entity.Ingredient;
 
 @Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Long>{}
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+	
+	@Query(value = "SELECT * FROM Ingredient u where u.name = LOWER(:name) LIMIT 1", nativeQuery = true)
+	Optional<Ingredient> findByName(@Param("name") String name);
+	
+}
